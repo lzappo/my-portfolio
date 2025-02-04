0bf26f5028e159b09b760639a9b7f175fa8195b1
Lui Zappitelli <luizappitelli@gmail.com>
2025-02-03 20:57:51 -0600

    resume page made and styled

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "../styles/ResumePage.css";
+import { FaDownload } from "react-icons/fa";
+import resumePDF from "../assets/LuiZappitelli_CV.pdf"; // Ensure this is in your assets folder
+import resumeImage from "../assets/resumeTemplate.png"; // A screenshot or exported image of your resume
 
 const ResumePage = () => {
-  return <div>ResumePage</div>;
+  return (
+    <section className="resume-section">
+      <h2 className="resume-title">My Resume</h2>
+
+      <a
+        href={resumePDF}
+        download="LuiZappitelli_CV.pdf"
+        className="btn resume-download"
+      >
+        <FaDownload /> Download CV
+      </a>
+
+      <div className="resume-preview">
+        <img
+          src={resumeImage}
+          alt="Lui Zappitelli Resume"
+          className="resume-image"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default ResumePage;
