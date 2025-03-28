00b9c54aa18a08d985d0827dce2aa1b13e1572cc
Lui Zappitelli <luizappitelli@Luis-MacBook-Pro.local>
2025-03-28 08:18:01 -0500

    refactored projects

---
+++ src/data/skills.jsx
@@ -0,0 +1,12 @@
+import { FaHtml5, FaCss3Alt } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiReact, SiTypescript } from "react-icons/si";
+
+const skills = [
+  { name: "HTML", icon: <FaHtml5 className="skill-icon" /> },
+  { name: "CSS", icon: <FaCss3Alt className="skill-icon" /> },
+  { name: "JavaScript", icon: <IoLogoJavascript className="skill-icon" /> },
+  { name: "TypeScript", icon: <SiTypescript className="skill-icon" /> },
+  { name: "React", icon: <SiReact className="skill-icon" /> },
+  { name: "React Native📱", icon: <SiReact className="skill-icon" /> }, // subtle but clear
+];
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -3,41 +3,7 @@ import "../styles/ProjectsPage.css";
 import worldImage from "../assets/world-image.svg";
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import DoneWithIt from "@/assets/DoneWithIt.png";
-
-const projects = [
-  {
-    title: "Moshify",
-    description: "A website built with just HTML and CSS alongside Mosh.",
-    image: "https://moshifyxlzappo.vercel.app/images/banner@2x.webp",
-    github: "https://github.com/lzappo/moshifyxlzappo",
-    demo: "https://moshifyxlzappo.vercel.app",
-    tags: ["HTML", "CSS", "Responsive Design"],
-  },
-  {
-    title: "Game-Hub",
-    description: "An intensive videogame database with filtering abilities",
-    image: "https://lui-game-hub.vercel.app/assets/logo-ff4914e6.webp",
-    github: "https://github.com/lzappo/game-hub",
-    demo: "https://lui-game-hub.vercel.app/",
-    tags: ["React", "TypeScript", "Rawg API"],
-  },
-  {
-    title: "My-Portfolio",
-    description: "This portfolio project :)",
-    image: worldImage,
-    github: "https://github.com/lzappo/my-portfolio",
-    demo: "https://luizappitelli.vercel.app",
-    tags: ["React", "Framer Motion", "Responsive"],
-  },
-  {
-    title: "DoneWithIt",
-    description:
-      "A mobile app that serves as a marketplace when you are simply done with it!",
-    image: DoneWithIt,
-    github: "https://github.com/lzappo/DoneWithIt",
-    tags: ["React Native", "Expo", "Mobile App"],
-  },
-];
+import projects from "@/data/projects";
 
 const ProjectsPage = () => {
   return (
