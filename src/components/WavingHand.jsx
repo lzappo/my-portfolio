879805968877750531b7075425ed792c920f7aac
Lui Zappitelli <luizappitelli@Luis-MacBook-Pro-2.local>
2025-04-01 19:06:38 -0500

    waving delay shortened

--- src/components/WavingHand.jsx
+++ src/components/WavingHand.jsx
@@ -7,7 +7,7 @@ const WavingHand = () => {
       aria-label="waving hand"
       initial={{ rotate: 0 }}
       animate={{ rotate: [0, 20, -10, 20, -5, 0] }}
-      transition={{ duration: 1.5, repeat: Infinity, repeatDelay: 3 }}
+      transition={{ duration: 1.5, repeat: Infinity, repeatDelay: 1 }}
       style={{ display: "inline-block", originX: 0.7, originY: 0.7 }}
     >
       👋🏼
