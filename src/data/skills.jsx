be4a9bbaad998551432f2367f67eab0265268297
Lui Zappitelli <luizappitelli@Luis-MacBook-Pro-2.local>
2025-04-07 09:58:51 -0500

    emoji removed in react native skill card

--- src/data/skills.jsx
+++ src/data/skills.jsx
@@ -8,7 +8,7 @@ const skills = [
   { name: "JavaScript", icon: <IoLogoJavascript className="skill-icon" /> },
   { name: "TypeScript", icon: <SiTypescript className="skill-icon" /> },
   { name: "React", icon: <SiReact className="skill-icon" /> },
-  { name: "React Native📱", icon: <SiReact className="skill-icon" /> }, // subtle but clear
+  { name: "React Native", icon: <SiReact className="skill-icon" /> }, // subtle but clear
 ];
 
 export default skills;
