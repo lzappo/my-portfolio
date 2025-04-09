c8cd0812721a8c036ee69b05ce6f6b7fcb19f1e2
Lui Zappitelli <luizappitelli@Luis-MacBook-Pro-2.local>
2025-04-09 10:44:33 -0500

    top and bottom padding now uniform

--- src/pages/ResumePage.jsx
+++ src/pages/ResumePage.jsx
@@ -46,7 +46,7 @@ const ResumePage = () => {
           src={resumeImage}
           alt="Lui Zappitelli Resume"
           className={`resume-image ${imageLoaded ? "visible" : "hidden"}`}
-          onLoad={() => setTimeout(() => setImageLoaded(true), 3000)}
+          onLoad={() => setImageLoaded(true)}
         />
       </motion.div>
 
